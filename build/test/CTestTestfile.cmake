e1423cf35276e8efe8d7d49207a0e4ef1d984ee5
svetlanlka <Flaffi_suny@mail.ru>
2021-03-12 23:22:17 +0300

    try to add CI-3

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/svetlana/tp/technopark_cpp/test
-# Build directory: /home/svetlana/tp/technopark_cpp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(date_parser_test "/home/svetlana/tp/technopark_cpp/build/test/date_parser_test_tests")
-set_tests_properties(date_parser_test PROPERTIES  _BACKTRACE_TRIPLES "/home/svetlana/tp/technopark_cpp/test/CMakeLists.txt;17;add_test;/home/svetlana/tp/technopark_cpp/test/CMakeLists.txt;0;")
